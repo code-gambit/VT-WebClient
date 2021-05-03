84493da3ca790c5f3c6716b148abdd83a92ae14b
Cipher2472 <ayushtiwari5747462@gmail.com>
2021-05-04 02:50:33 +0530

    Fixed #5

--- src/App.js
+++ src/App.js
@@ -1,56 +1,20 @@
-import {BrowserRouter, Route, Switch, useHistory} from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom'
 
 import Header from "./Components/Header";
-import Homepage from "./Components/Screens/Homepage";
-import Dashboard from "./Components/Screens/Dashboard";
-import FileDetail from "./Components/Screens/FileDetail";
-import Files from "./Components/Screens/Files";
-import Profile from "./Components/Screens/Profile";
-import About from './Components/Screens/About';
+import Routing from "./Routing"
 
 import './App.css';
 import AuthContextProvider from './Context/Contexts/AuthContext';
 import FileContextProvider from './Context/Contexts/FileContext';
 require('dotenv').config()
-const Routing= () =>{
-  const history=useHistory()
-  const auth = JSON.parse(localStorage.getItem("auth"));
-  if(!auth){
-    if(!(history.location.pathname="/") && !(history.location.pathname="/about")){
-      history.push("/");
-    }
-  }
 
-  return(
-    <Switch>
-      <Route exact path="/">
-        <Homepage/>
-      </Route>
-      <Route exact path="/dashboard">
-        <Dashboard/>
-      </Route>
-      <Route exact path="/files">
-        <Files/>
-      </Route>
-      <Route exact path="/file/:fileId">
-        <FileDetail/>
-      </Route>
-      <Route exact path="/profile">
-        <Profile/>
-      </Route>
-      <Route exact path="/about">
-        <About/>
-      </Route>
-    </Switch>
-  )
-}
 
 function App() {
   return (
     <div className="App">
       <AuthContextProvider>
-        <BrowserRouter>          
-          <FileContextProvider>          
+        <BrowserRouter>
+          <FileContextProvider>
             <Header/>
             <Routing/>
           </FileContextProvider>
---
+++ src/Routing.js
@@ -0,0 +1,46 @@
+import Homepage from "./Components/Screens/Homepage";
+import Dashboard from "./Components/Screens/Dashboard";
+import FileDetail from "./Components/Screens/FileDetail";
+import Files from "./Components/Screens/Files";
+import Profile from "./Components/Screens/Profile";
+import About from "./Components/Screens/About";
+import { useContext } from "react";
+import { Redirect, Route, Switch, useHistory } from "react-router-dom";
+import { AuthContext } from "./Context/Contexts/AuthContext";
+
+const Routing = () => {
+  const history = useHistory();
+  const { authState } = useContext(AuthContext);
+  const auth = JSON.parse(localStorage.getItem("auth"));
+  if (!auth) {
+    if (
+      history.location.pathname !== "/" &&
+      history.location.pathname !== "/about"
+    ) {
+      history.push("/");
+    }
+  }
+  return (
+    <>
+      {authState.auth.PK ? (
+        <Switch>
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/files" component={Files} />
+          <Route exact path="/file/:fileId" component={FileDetail} />
+          <Route exact path="/profile" component={Profile} />
+          <Route exact path="/about" component={About} />
+          <Route>
+            <Redirect to="/dashboard" /> <Dashboard />
+          </Route>
+        </Switch>
+      ) : (
+        <Switch>
+          <Route exact path="/about" component={About} />
+          <Route exact path="/" component={Homepage} />
+        </Switch>
+      )}
+    </>
+  );
+};
+
+export default Routing;
