8e128a87703aa139e7ca54d7c9435762d6f8e421
Cipher2472 <ayushtiwari5747462@gmail.com>
2021-05-06 01:56:17 +0530

    resolved test fails

--- src/Components/Screens/FileForm.js
+++ src/Components/Screens/FileForm.js
@@ -1,8 +1,18 @@
 import React, { useContext, useState } from 'react';
 import * as FileActionCreators from '../../Context/ActionCreators/FileActionCreator';
+import * as AuthActionCreators from '../../Context/ActionCreators/AuthActionCreater';
 import { FileContext } from '../../Context/Contexts/FileContext';
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 import { AuthContext } from '../../Context/Contexts/AuthContext';
+import axios from 'axios'
+
+if((typeof TextDecoder==='undefined' || typeof TextEncoder==='undefined') && typeof require!=='undefined'){
+  global.TextDecoder = require('util').TextDecoder
+  global.TextEncoder = require('util').TextEncoder
+}
+
+const ipfs=require("ipfs-http-client")
+const client=ipfs({host:'ipfs.infura.io',port:5001,protcol:'https'})
 
 const FileForm = () => {
     const {fileDispatch} = useContext(FileContext)
@@ -11,24 +21,53 @@ const FileForm = () => {
     const [hash,setHash] = useState("");
     const [type,setType] = useState("");
     const [file,setFile] = useState("");
-    const handleSubmit = (e)=>{
-        e.preventDefault();
-        var fileData={
-            PK:authState.auth.PK,
-            SK:"FILE#12345",
-            LS1_SK:name,
-            hash:"123456",
-            size:12,
-            type:type,
+    async function getBuffer(file){
+      const reader=new window.FileReader();
+      return new Promise((resolve,reject)=>{
+        reader.readAsArrayBuffer(file);
+        reader.onloadend= async ()=>{
+          const buffer= await Buffer(reader.result)
+          resolve(buffer);
         }
-        FileActionCreators.addFile(fileData,fileDispatch);
+      })
+    }
+
+    const handleSubmit = async (e)=>{
+        e.preventDefault();
+        const buffer=await getBuffer(e.target[2].files[0])
+        const res=await client.add(buffer)
+        const fileSize=(e.target[2].files[0].size)/1048576
+        var fileData={
+          f_type:e.target[1].value,
+          hash:res.path,
+          size:parseFloat(fileSize.toFixed(4)),
+          LS1_SK: e.target[0].value
+        }
+        const userId = JSON.parse(localStorage.getItem("auth")).PK.substring(5);
+        axios.post(
+            `${process.env.REACT_APP_BACKENDURL}/user/${userId}/file`, fileData,
+            {
+                headers:{
+                    'X-Api-Key':process.env.REACT_APP_APIKEY,
+                },
+
+            }
+        ).then((response)=>{
+            setFile(response.data.body)
+            FileActionCreators.addFile(fileData,fileDispatch);
+            authState.auth.storage_used+=fileData.size
+            authState.auth.storage_used=parseFloat(authState.auth.storage_used.toFixed(4))
+            AuthActionCreators.authStateUpdate(authState)
+        }, (error) => {
+            console.log(error);
+        })
         setName("");
         setHash("");
         setType("");
         setFile("");
         //window.location.reload(false);    //enable this when not using mock data
     }
-    return ( 
+    return (
         // <form onSubmit={handleSubmit}>
         //     <input type="text" value={name} onChange={(e)=>{setName(e.target.value)}} placeholder="File Name"/>
         //     <input type="text" value={hash} onChange={(e)=>{setHash(e.target.value)}} placeholder="File Hash"/>
@@ -38,7 +77,7 @@ const FileForm = () => {
         <Form onSubmit={handleSubmit}>
             <FormGroup>
                 <Label for="fileName">File Name</Label>
-                <Input type="text" name="email" id="fileName" value={name} placeholder="File Name" 
+                <Input type="text" name="email" id="fileName" value={name} placeholder="File Name"
                     onChange={(e) => {setName(e.target.value)}}
                 />
             </FormGroup>
@@ -63,5 +102,5 @@ const FileForm = () => {
         </Form>
      );
 }
- 
+
 export default FileForm;
